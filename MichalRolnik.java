2fc33ab60b1ec85e45fbc5c719e2ed7d82542abd
Michal <rolnikmichal@gmail.com>
2024-05-26 16:22:24 +0300

    NOW added to the text

--- MichalRolnik.java
+++ MichalRolnik.java
@@ -2,6 +2,9 @@ public class MichalRolnik {
     public static void main(String[] args){
         
             System.out.println("Bring them home");
+            System.out.println(" NOW!");
+
+
         
 
             
